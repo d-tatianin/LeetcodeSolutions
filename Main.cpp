4802057ec9b629c28217ab39a2eb1d5fdd696ee7
8infy <99danilt@gmail.com>
2021-01-16 15:01:48 +0300

    Added a solution for problem 172.

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
+#include "172-factorial-trailing-zeroes.h"
 #include "371-sum-of-two-integers.h"
 
 
 int main()
 {
+    auto zeroes = problem_172::trailing_zeroes(100);
     auto sum = problem_371::sum(333, 666);
 }
---
+++ Solutions/172-factorial-trailing-zeroes.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// 172. Given an integer n, return the number of trailing zeroes in n!.
+
+// Solution:
+// Since trailing zeroes come from 10, which in turn comes from 5 * 2, we need to calculate how many
+// such multiplications a factorial includes. We can do this by dividing n by five until n is
+// greater than or equal to 5.
+
+namespace problem_172 {
+
+inline int trailing_zeroes(int n)
+{
+    int zeroes = 0;
+
+    while (n >= 5) {
+        n /= 5;
+        zeroes += n;
+    }
+
+    return zeroes;
+}
+
+}
