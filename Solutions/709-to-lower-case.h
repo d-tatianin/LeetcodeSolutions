666cd817953adb4160ffcff8561bf6837d02acf7
8infy <99danilt@gmail.com>
2021-01-19 01:01:06 +0300

    Added a solution for problem 709

---
+++ Solutions/709-to-lower-case.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// 709. Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+// Solution: (complexity O(N))
+// Just walk the string and for each char in the range A-Z subtract an offset
+// needed to convert it to lower case.
+
+namespace problem_709 {
+
+inline std::string to_lower(std::string str) {
+    // This makes an assumption that characters are ASCII, or at least
+    // the fact that the upper case chars have higher codepoints and are contiguos.
+    // Alternatively you could set up a hash map for all english letters of mappings from upper to lower.
+    static constexpr auto offset_from_lower_case = 'A' - 'a';
+
+    for (auto& c : str) {
+        if (c >= 'A' && c <= 'Z')
+            c -= offset_from_lower_case;
+    }
+
+    return str;
+}
+
+}
