de9dbc95676f16ffd80b592342ead2b5e4718834
8infy <99danilt@gmail.com>
2021-01-20 00:24:16 +0300

    Added a solution for problem 941.

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,10 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
+#include "3-longest-substring-without-repeating.h"
 #include "15-3sum.h"
 #include "16-3sum-closest.h"
+#include "116-populating-next-right.h"
 #include "172-factorial-trailing-zeroes.h"
 #include "263-ugly-number.h"
 #include "264-ugly-number-2.h"
@@ -20,10 +22,25 @@ struct TreeNode {
 #include "762-partition-labels.h"
 #include "819-most-common-word.h"
 #include "836-rectangle-overlap.h"
+#include "941-valid-mountain-array.h"
 #include "1010-pair-of-songs-divisible-by-60.h"
 #include "1275-find-winner-tic-tac-toe.h"
 #include "1344-angle-between-hands-of-clock.h"
 #include "1544-make-the-string-great.h"
+
+void try_116()
+{
+    using Node = problem_116::Node;
+
+    Node n  { 1 };
+    Node n1 { 2 };
+    Node n2 { 3 };
+
+    n.left = &n1;
+    n.right = &n2;
+
+    problem_116::connect(&n);
+}
 
 void try_297()
 {
@@ -45,9 +62,11 @@ void try_297()
 
 int main()
 {
+    auto len = problem_3::length_of_longest_substring("abcabcbb");
     std::vector<int> vec = { 3, -2, -1 };
     auto tsum = problem_15::three_sum(vec);
     auto tsumc = problem_16::three_sum_closest(vec, 5);
+    try_116();
     auto zeroes = problem_172::trailing_zeroes(100);
     auto is_ugly = problem_263::is_ugly_number(15);
     auto nth_ugly = problem_264::nth_ugly_number(10);
@@ -59,6 +78,7 @@ int main()
     auto lower = problem_709::to_lower("HELLO World");
     auto word = problem_819::most_common_word("hello hello word", { "hello" });
     auto ovrl = problem_836::rects_overlap({ 0,0,2,2 }, { 1,1,3,3 });
+    auto isval = problem_941::valid_mountain_array({ 0,3,2,1 });
     auto vals = problem_1010::pair_count_divisible_by_60({ 30,20,150,100,40 });
     auto res = problem_1275::play_tictactoe({ {0, 0}, { 1, 1 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 2, 0 } });
     auto deg = problem_1344::angle_between_clock_hands(12, 30);
---
+++ Solutions/941-valid-mountain-array.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+
+// 941. Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+// Solution: (complexity O(N))
+// Walk up until we meet a number that's not greater than the previous.
+// If we stopped at index 1 (aka haven't moved) or at index == size (aka moved to the end) return false, as it's invalid.
+// Walk down until we meet a number that's not less than the previous.
+// If we ended up at index == size it's a valid mountaint array, otherwise we encounter an invalid number.
+
+namespace problem_941 {
+
+inline bool valid_mountain_array(const std::vector<int>& arr) {
+    if (arr.size() < 3)
+        return false;
+
+    size_t index = 1;
+
+    while (index < arr.size() && arr[index - 1] < arr[index])
+        ++index;
+
+    if (index == 1 || index == arr.size())
+        return false;
+
+    while (index < arr.size() && arr[index - 1] > arr[index])
+        ++index;
+
+    return index == arr.size();
+}
+
+}
