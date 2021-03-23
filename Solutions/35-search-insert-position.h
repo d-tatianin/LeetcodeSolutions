7ad9c10ea0ce1c5883f28f7673a976e3de31a7d6
8infy <99danilt@gmail.com>
2021-03-23 21:52:04 +0300

    Added a solution for problem 35

---
+++ Solutions/35-search-insert-position.h
@@ -0,0 +1,38 @@
+#pragma once
+
+// 35. Given a sorted array of distinct integers and a target value, return the index if the target is found.
+//     If not, return the index where it would be if it were inserted in order.
+
+// Solution: (complexity O(Log N))
+// Classic binary search.
+
+SOLUTION_FOR_PROBLEM(35, "ProblemName") {
+
+int searchInsert(std::vector<int>& nums, int target) {
+    if (nums.empty())
+        return 0;
+
+    int begin = 0;
+    int end = static_cast<int>(nums.size()) - 1;
+
+    while (begin <= end) {
+        auto middle = begin + (end - begin) / 2;
+
+        if (nums[middle] < target)
+            begin = middle + 1;
+        else if (target < nums[middle])
+            end = middle - 1;
+        else
+            return middle;
+    }
+
+    return begin;
+}
+
+SOLVE()
+{
+    std::vector<int> vec = { 1, 2, 999, 99999 };
+    auto res = searchInsert(vec, 3);
+}
+
+}
